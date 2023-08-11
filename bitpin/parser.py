d728a9a585f0f8c330075949040e45d0e63760ca
Omid Mesgarha <omid.mesgarha@tapsi.cap>
2023-08-11 16:44:00 +0330

    parsing data from bitpin

---
+++ bitpin/parser.py
@@ -0,0 +1,24 @@
+import json, time, datetime
+from hashlib import md5
+def get_symbols_parser(data) -> []:
+    results = []
+    for market in data['results']:
+        if 'otc_market' in market and market['otc_market'] == False:
+            results.append({
+                'id': market['id'],
+                'base': market['currency1']['code'],
+                'qoute': market['currency2']['code']
+                            })
+    return results
+
+def get_last_trades_parser(data):
+    matches = []
+    for match in data:
+        matches.append({
+            'time': datetime.datetime.fromtimestamp(int(match['time'])).isoformat(),
+            'price': match['price'],
+            'amount': match['match_amount'],
+            'type': match['type'],
+            'unifier': md5(match['match_id'].encode()).hexdigest()[:10]
+        })
+    return matches
