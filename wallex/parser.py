1a6ebf973866175394a9ed49324b415be9e6a20d
Omid Mesgarha <omid.mesgarha@tapsi.cap>
2023-09-04 00:40:30 +0330

    parse the api response of last trades for wallex

--- wallex/parser.py
+++ wallex/parser.py
@@ -1,6 +1,22 @@
-import json
+import json, datetime
+from hashlib import md5
+
 def las_trade_parser(data):
-    print(json.dumps(data, indent=3))
+    raw_trades = data['result']['latestTrades']
+    trades = []
+    for raw_trade in raw_trades:
+        trades.append(
+            {
+                'time': datetime.datetime.strptime(raw_trade['timestamp'], '%Y-%m-%dT%H:%M:%SZ').isoformat(),
+                'price': round(float(raw_trade['price']), 3),
+                'amount': float(raw_trade['quantity']),
+                'type': 'buy' if raw_trade['isBuyOrder'] == 'true' else 'sell',
+                'market_name': raw_trade['symbol'].replace('TMN', 'IRT'),
+                'source': 'wallex',
+                'unifier': md5((raw_trade['timestamp'] + raw_trade['quantity']).encode()).hexdigest()[:10]
+            }
+        )
+    return trades
 
 def get_symbols_parser(data, filter: str = []):
     symbols = data["result"]['symbols']
