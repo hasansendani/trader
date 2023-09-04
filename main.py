30889928a1c8f240df521aaa8016566f51a457e6
armagg <omid.mesgarha@gmail.com>
2023-09-04 19:44:18 +0330

    testing wallex

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from bitpin.api_services import  get_last_trades as last_bitpin
 from bitpin.parser import get_last_trades_parser as last_trade_parser_bitpin
 from bitpin.markets import markets
 from dbservice import write
+from wallex.api_service import get_last_trade as last_wallex, get_symbols 
+from wallex.parser import las_trade_parser as last_trade_parser_wallex , get_symbols_parser
 
 
 async def get_bitpin_data():
@@ -21,11 +23,15 @@ async def call_and_save_bitpin( key, val):
     
     
 async def main():
-    scheduler = AsyncIOScheduler()
-    scheduler.add_job(get_bitpin_data, 'interval', minutes=2)
-    scheduler.start()
-    while True:
-        await asyncio.sleep(1000)
+    data = await get_symbols()
+    get_symbols_parser(data, ['TMN'])
+    data = await last_wallex('BTCTMN')
+    last_trade_parser_wallex(data)
+    # scheduler = AsyncIOScheduler()
+    # scheduler.add_job(get_bitpin_data, 'interval', minutes=2)
+    # scheduler.start()
+    # while True:
+    #     await asyncio.sleep(1000)
 
 
 if __name__ == "__main__":
