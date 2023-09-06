e1a8de23bdc52c84f2d5b1d10f766980d71821d4
armagg <omid.mesgarha@gmail.com>
2023-09-07 02:28:13 +0330

    close db connection gracefully

--- dbservice.py
+++ dbservice.py
@@ -2,13 +2,15 @@ from motor.motor_asyncio import AsyncIOMotorClient
 from redis import Redis
 from decouple import config
 
-client: AsyncIOMotorClient = AsyncIOMotorClient(config("MONGO_HOST"))
-db = client.market_making
-collection = db.last_trades
 redis_client = Redis(config('REDIS_HOST'), port=6379, db=0)
 ttl = int(config('REDIS_TTL'))
 
 async def write(document):
+    client: AsyncIOMotorClient = AsyncIOMotorClient(config("MONGO_HOST"))
+    db = client.market_making
+    collection = db.last_trades
+
+
     unifier = document['unifier']
     if not redis_client.exists(unifier):
         try:
@@ -16,5 +18,7 @@ async def write(document):
             redis_client.setex(unifier, ttl, 1)
         except Exception as e:
             print(e)
+        finally:
+            client.close()
     else: 
         raise ValueError()
