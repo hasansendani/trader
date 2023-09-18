8bdbb1723acff4ff5a544ff3857aba4f84b85fee
armagg <omid.mesgarha@gmail.com>
2023-09-19 02:01:39 +0330

    add encoding for responses

--- ramzinex/api_services.py
+++ ramzinex/api_services.py
@@ -6,7 +6,7 @@ BASE_URL = "https://publicapi.ramzinex.com/exchange/api/v1.0/exchange/"
 async def get_data(url):
    async with ClientSession() as session:
        async with session.get(url) as resp:
-           data = await resp.json()
+           data = await resp.json(encoding ='utf-8')
            if resp.status == 200 and 'data' in data:
                return data['data']
 
