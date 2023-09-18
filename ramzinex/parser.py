42a4e7accc8b0dae6b302cf27129c79fbd0026e3
armagg <omid.mesgarha@gmail.com>
2023-09-19 02:02:42 +0330

    handle for in case of empty lists

--- ramzinex/parser.py
+++ ramzinex/parser.py
@@ -10,14 +10,18 @@ def get_symbols_parser(data, filter: list[str] = []):
 
 def get_recent_parser(data, market_name):
     matches = []
-    for match in data:
-        matches.append({
-            "time": datetime.datetime.strptime(match[2], '%Y-%m-%d %H:%M:%S').isoformat(),
-            "price": match[0],
-            "amount": match[1],
-            "type": match[3],
-            "market_name": market_name,
-            "source": "ramzinex",
-            "unifier": match[5][:10]
-        })
-    return matches
+
+    if data:
+        for match in data:
+            matches.append({
+                "time": datetime.datetime.strptime(match[2], '%Y-%m-%d %H:%M:%S').isoformat(),
+                "price": match[0],
+                "amount": match[1],
+                "type": match[3],
+                "market_name": market_name,
+                "source": "ramzinex",
+                "unifier": match[5][:10]
+            })
+        return matches
+    else:
+        return []
