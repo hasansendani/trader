8bcf5f574c727b4f2fa0d6bbbd4dfd12d11af7ca
armagg <omid.mesgarha@gmail.com>
2023-09-19 13:21:40 +0330

    bug fix in type of json decoder

--- wallex/parser.py
+++ wallex/parser.py
@@ -9,7 +9,7 @@ def last_trade_parser(data):
                 'time': datetime.datetime.strptime(raw_trade['timestamp'], '%Y-%m-%dT%H:%M:%SZ').isoformat(),
                 'price': round(float(raw_trade['price']), 3),
                 'amount': float(raw_trade['quantity']),
-                'type': 'buy' if raw_trade['isBuyOrder'] == 'true' else 'sell',
+                'type': 'buy' if raw_trade['isBuyOrder'] else 'sell',
                 'market_name': raw_trade['symbol'].replace('TMN', 'IRT'),
                 'source': 'wallex',
                 'unifier': md5((raw_trade['timestamp'] + raw_trade['quantity']).encode()).hexdigest()[:10]
