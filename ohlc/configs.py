b9527d000280fa4d9a6c5ce86eda805eaf2a528c
armagg <omid.mesgarha@gmail.com>
2024-08-23 13:42:03 +0330

    change depricated time intervals

--- ohlc/configs.py
+++ ohlc/configs.py
@@ -1,12 +1,12 @@
-LIST_OF_SOURCES = ['ramzinex', 'nobitex', 'bitpin', 'wallex']
+LIST_OF_SOURCES = ['ramzinex', 'nobitex', 'wallex', 'bitpin']
 DB_NAME = "market_making"
 COLLECTION_NAME = "last_trades"
 OHLC_COLLECTION_NAME = "ohlc"
 INTERVALS = {
     '1D': '1D',
-    '4H': '4H',
-    '1H': '1H',
-    '15Min': '15T',
-    '5Min': '5T',
-    '1Min': '1T'
+    '4H': '4h',
+    '1H': '1h',
+    '15Min': '15min',
+    '5Min': '5min',
+    '1Min': '1min'
 }
