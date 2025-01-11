6210e2498e9d30e275b70124fc9ffbde457f57a6
armagg <omid.mesgarha@gmail.com>
2025-01-11 18:24:35 +0330

    add tabdeal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import asyncio
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
-from apscheduler.executors.pool import ThreadPoolExecutor
 from bitpin.api_services import get_last_trades as last_bitpin
 from bitpin.parser import get_last_trades_parser as last_trade_parser_bitpin
 from bitpin.markets import markets
@@ -8,7 +7,6 @@ from dbservice import write
 from wallex.api_service import get_last_trade as last_wallex, get_symbols
 from wallex.parser import last_trade_parser as last_trade_parser_wallex, \
     get_symbols_parser
-from datetime import datetime
 from ramzinex.api_services import get_last_trades as last_trades_ramzinex, \
     get_symbols as get_symbols_ramzinex
 from ramzinex.parser import get_recent_parser as get_recent_parser_ramzinex, \
@@ -93,13 +91,35 @@ async def get_nobitex_data():
     await asyncio.gather(*tasks)
 
 
+async def get_tabdeal_data():
+    from tabdeal.api_services import get_symbols
+    from tabdeal.parser import get_symbols_parser
+    symbols = get_symbols_parser(await get_symbols())
+    tasks = []
+    for market_name, market_id in symbols.items():
+        tasks.append(call_and_save_tabdeal(market_id, market_name))
+    await asyncio.gather(*tasks)
+
+
+async def call_and_save_tabdeal(market_id, market_name):
+    from tabdeal.api_services import get_last_trades
+    from tabdeal.parser import get_last_trade_parser
+    last_trades = get_last_trade_parser(await get_last_trades(market_id),
+                                        market_name)
+    for trade in last_trades:
+        try:
+            await write(trade)
+        except ValueError:
+            break
+
+
 async def main():
     scheduler = AsyncIOScheduler()
     scheduler.add_job(get_bitpin_data, 'interval', minutes=2)
     scheduler.add_job(get_wallex_data, 'interval', minutes=1)
     scheduler.add_job(get_ramzinx_data, 'interval', minutes=1)
     scheduler.add_job(get_nobitex_data, 'interval', minutes=1)
-
+    scheduler.add_job(get_tabdeal_data, 'interval', minutes=3)
     scheduler.start()
     event = asyncio.Event()
     await event.wait()
